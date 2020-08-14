28fa428b7885f095fca57edd0bcd7a64839ac3dd
Minghan Zhu <minghanz@umich.edu>
2020-08-14 19:24:11 -0400

    package for installation

---
+++ setup.py
@@ -0,0 +1,53 @@
+
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+setup(
+    name='c3d',
+    description='Package for Continuous 3D Loss',
+    author='Minghan Zhu',
+    author_email='minghanz@umich.edu',
+    url='https://github.com/minghanz/c3d',
+    ext_modules=[
+        CUDAExtension('c3d.cvo_ops.sub_cuda', [
+            'c3d/cvo_ops/custom_ori/sub_cuda.cpp',
+            'c3d/cvo_ops/custom_ori/sub_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.sub_norm_cuda_half_paral', [
+            'c3d/cvo_ops/custom_norm/sub_norm_cuda.cpp',
+            'c3d/cvo_ops/custom_norm/sub_norm_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.cvo_dense_with_normal_output', [
+            'c3d/cvo_ops/custom_dense_with_normal/cvo_dense_with_normal_cuda.cpp',
+            'c3d/cvo_ops/custom_dense_with_normal/cvo_dense_with_normal_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.cvo_dense_normal', [
+            'c3d/cvo_ops/custom_dense_normal/cvo_dense_normal_cuda.cpp',
+            'c3d/cvo_ops/custom_dense_normal/cvo_dense_normal_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.cvo_dense_dist', [
+            'c3d/cvo_ops/custom_dense_dist/cvo_dense_samp_cuda.cpp',
+            'c3d/cvo_ops/custom_dense_dist/cvo_dense_samp_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.cvo_dense_angle', [
+            'c3d/cvo_ops/custom_dense_angle/cvo_dense_angle_cuda.cpp',
+            'c3d/cvo_ops/custom_dense_angle/cvo_dense_angle_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.cvo_dense_samp', [
+            'c3d/cvo_ops/custom_dense/cvo_dense_samp_cuda.cpp',
+            'c3d/cvo_ops/custom_dense/cvo_dense_samp_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.cross_subtract_cuda', [
+            'c3d/cvo_ops/custom_cross_subtract/cross_subtract_cuda.cpp',
+            'c3d/cvo_ops/custom_cross_subtract/cross_subtract_cuda_kernel.cu',
+        ]),
+        CUDAExtension('c3d.cvo_ops.cross_prod_cuda', [
+            'c3d/cvo_ops/custom_cross_prod/cross_prod_cuda.cpp',
+            'c3d/cvo_ops/custom_cross_prod/cross_prod_cuda_kernel.cu',
+        ]),
+        
+    ],
+    packages=find_packages(),
+    cmdclass={
+        'build_ext': BuildExtension
+    })
