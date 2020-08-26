c7bb3973759558e2b8aa8a245d7167f4e6a00e7f
Minghan <minghanz@umich.edu>
2020-08-26 18:05:36 -0400

    __init__.py for easier import

---
+++ c3d/__init__.py
@@ -0,0 +1,4 @@
+from .c3d_loss import C3DLoss
+from .pho_loss import PhoLoss
+import utils_general
+import utils
---
+++ c3d/utils/__init__.py
@@ -0,0 +1,6 @@
+from .cam import *
+from .cam_proj import CamInfo, CamProj
+from .dataset_kitti import *
+from .geometry import *
+from .io import *
+from .pc3d import *
